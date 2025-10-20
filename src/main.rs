bf74880096fd1556704e34bbbad5ccf75fbde908
Job Verhaak <job_verhaak@hotmail.com>
2025-10-20 19:43:49 +0200

    added config.toml and how to read the config from it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Deserialize)]
+struct AppConfig{
+    watch_path:String,
+}
+
+fn load_config() -> AppConfig {
+    let content = fs::read_to_string("config.toml").expect("Could not find config.toml in the working directory");
+    toml::from_str(&content).expect("TOML is invalid")
+}
+
+#[tokio::main]
+async fn main() {
+    let config = load_config();
+
+    println!("{}", config.watch_path);
 }
