a3861a3a2b2c4686448b3e7f52626f970ab19e45
Job Verhaak <job_verhaak@hotmail.com>
2025-10-21 21:55:37 +0200

    Able to reader the headers of CSVs in a configured folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use csv::ReaderBuilder;
 use serde::Deserialize;
-use std::fs;
+use std::{fs, path::PathBuf};
 
 #[derive(Deserialize)]
 struct AppConfig{
@@ -11,9 +12,29 @@ fn load_config() -> AppConfig {
     toml::from_str(&content).expect("TOML is invalid")
 }
 
+fn convert_csv(file_path: &PathBuf) {
+    let mut rdr = ReaderBuilder::new()
+        .flexible(true)
+        .from_path(file_path)
+        .expect("Unable to read CSV file");
+    let headers = rdr.headers().unwrap().clone();
+    println!("{:?}", headers);
+}
+
+fn files_in_input(){
+
+    let x = fs::read_dir("./input").expect("Cannot read files in /input");
+    for path in x {
+        //println!("{:?}",path.unwrap().path().display());
+        let file_path = path.unwrap().path();
+        convert_csv(&file_path);
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let config = load_config();
+    files_in_input();
 
     println!("{}", config.watch_path);
 }
