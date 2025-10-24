c86dc8d6ce670ced00f5eca677c60ac405bbf2eb
Job Verhaak <job_verhaak@hotmail.com>
2025-10-24 19:46:29 +0200

    Progress made in converting the csv to json

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use csv::ReaderBuilder;
 use serde::Deserialize;
+use serde_json::{Value, json};
 use std::{collections::HashMap, fs, path::PathBuf};
 
 #[derive(Deserialize)]
-struct AppConfig{
-    watch_path:String,
+struct AppConfig {
+    watch_path: String,
     root_name: Option<String>,
     mappings: HashMap<String, String>,
 }
 
 fn load_config() -> AppConfig {
-    let content = fs::read_to_string("config.toml").expect("Could not find config.toml in the working directory");
+    let content = fs::read_to_string("config.toml")
+        .expect("Could not find config.toml in the working directory");
     toml::from_str(&content).expect("TOML is invalid")
 }
 
@@ -20,15 +22,46 @@ fn convert_csv(file_path: &PathBuf, config: &AppConfig) {
         .from_path(file_path)
         .expect("Unable to read CSV file");
     let headers = rdr.headers().unwrap().clone();
-    println!("{:?}", headers);
-    for header in &headers {
-        let mapped_header = config.mappings.get(header).unwrap();
-        println!("{:?}", mapped_header);
+    let mut records = Vec::new();
+
+    for res in rdr.records() {
+        let record = res.unwrap();
+        let mut root = json!({});
+
+        for (i, value) in record.iter().enumerate() {
+            let header = &headers[i];
+            let mapped_key = config.mappings.get(header).unwrap(); // TODO proper error handling
+            // Function here to make the actual record and add to root.
+            insert_in_root(&mut root, mapped_key, value);
+        }
+        records.push(root)
+    }
+    println!("{:?}", records);
+}
+
+fn insert_in_root(root: &mut Value, key: &str, value: &str) {
+    // TODO Somehow im now missing column 1 and 2 and the values.
+    let mut root = root;
+    let header_parts: Vec<&str> = key.split('.').collect();
+
+    for (i, key) in header_parts.iter().enumerate() {
+        //check if its the last, then it needs to add value and not just a new json
+        if i == header_parts.len() - 1 {
+            if let Some(obj) = root.as_object_mut() {
+                obj.insert((*key).to_string(), json!(value));
+            }
+            println!("Last Key {}", key);
+        } else {
+            if let Some(obj) = root.as_object_mut() {
+                obj.insert((*key).to_string(), json!({}));
+            }
+            // Make sure to get the root as the new made root to nest it.
+            root = root.get_mut(*key).unwrap();
+        }
     }
 }
 
-fn files_in_input(){
-
+fn files_in_input() {
     let x = fs::read_dir("./input").expect("Cannot read files in /input");
     for path in x {
         //println!("{:?}",path.unwrap().path().display());
